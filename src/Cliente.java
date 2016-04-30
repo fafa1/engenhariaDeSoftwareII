e45fd576eaa08998e5ac6716dfd50d9e39be4ad9
Bruno Novaes Camera Santos <bcamera@hotmail.com>
2016-04-29 22:26:50 -0300

    Metodos Funcionario

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,5 +1,21 @@
 /**
  * Created by hohenheim on 4/29/16.
  */
-public class Cliente {
+public class Cliente extends Pessoa {
+	
+	/**
+	 * @author Bruno 
+	 */
+	public void visualizarHistóricoAluguel()
+	{
+		
+	}
+	
+	/**
+	 * @author Bruno 
+	 */
+	public void visualizarHistoricodeCompras()
+	{
+		
+	}
 }
--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,5 +1,101 @@
 /**
  * Created by hohenheim on 4/29/16.
  */
-public class Funcionario {
+public class Funcionario extends Pessoa {
+	
+	/**
+	 * @author Bruno 
+	 */
+	public void cadastrarVeiculo()
+	{
+		
+	}
+
+	/**
+	 * @author Bruno 
+	 */
+	public void cadastrarPlanos()
+	{
+		
+	}
+	
+	/**
+	 * @author Bruno 
+	 */
+	public void visualizarAgendamentos()
+	{
+		
+	}
+	
+	/**
+	 * @author Bruno 
+	 */
+	public void realizarCheckinDeLocacao()
+	{
+		
+	}
+	
+	/**
+	 * @author Bruno 
+	 */
+	public void realizarCheckoutDeLocacao()
+	{
+		
+	}
+	
+	/**
+	 * @author Bruno 
+	 */
+	public void visualizarFeedbacks()
+	{
+		
+	}
+	
+	/**
+	 * @author Bruno 
+	 */
+	public void cadastarPostos()
+	{
+		
+	}
+	
+	/**
+	 * @author Bruno 
+	 */
+	public void manterEstoqueVeiculos()
+	{
+		
+	}
+	
+	/**
+	 * @author Bruno 
+	 */
+	public void cadastrarPlanosdePagamento()
+	{
+		
+	}
+	
+	/**
+	 * @author Bruno 
+	 */
+	public void venderPecas()
+	{
+		
+	}
+	
+	/**
+	 * @author Bruno 
+	 */
+	public void visualizarAgendamento()
+	{
+		
+	}
+	
+	/**
+	 * @author Bruno 
+	 */
+	public void cadastrarPecas()
+	{
+		
+	}
 }
